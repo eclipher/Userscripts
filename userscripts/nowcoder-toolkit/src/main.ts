b7e5488aee34cb0f7300071d470b5f54e9e01598
default <kay7942001@gmail.com>
2025-05-29 15:17:21 +0000

    fix(nowcoder): unf**k `addEventListner`

--- userscripts/nowcoder-toolkit/src/main.ts
+++ userscripts/nowcoder-toolkit/src/main.ts
@@ -1,10 +1,13 @@
 import { mount } from "svelte";
 import { Toaster } from "$lib/toast";
 import { practicePage } from "./routes/practice";
+import { correctAddEventListener } from "$lib/correct-event-listner";
 
 mount(Toaster, {
     target: document.body,
     props: { position: "top-center" },
 });
+
+correctAddEventListener();
 
 practicePage();
