ec211d36e94b27488965c1ef57dd9cce35a72843
eclipher <131247135+eclipher@users.noreply.github.com>
2025-05-30 05:50:00 +0000

    feat(nowcoder): save as jupyter notebook

--- userscripts/nowcoder-toolkit/src/routes/practice/ProblemButtons.svelte
+++ userscripts/nowcoder-toolkit/src/routes/practice/ProblemButtons.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import ButtonGroups from "$components/ButtonGroup.svelte";
-    import CopyDescription from "./CopyDescription.svelte";
+    import CopyDescription from "./copy-description.svelte";
+    import SaveAsJupyter from "./save-as-jupyter.svelte";
 </script>
 
-<ButtonGroups style="padding: 0px 10px 10px"><CopyDescription /></ButtonGroups>
+<ButtonGroups style="padding: 0px 10px 10px">
+    <CopyDescription />
+    <SaveAsJupyter />
+</ButtonGroups>
