a47735c2fbd7543184362cafcdb1beed07a5c327
eclipher <131247135+eclipher@users.noreply.github.com>
2025-05-30 06:04:41 +0000

    chore(nowcoder): release v1.0.0

--- userscripts/nowcoder-toolkit/vite.config.ts
+++ userscripts/nowcoder-toolkit/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import monkey from "vite-plugin-monkey";
+import monkey, { cdn } from "vite-plugin-monkey";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -10,15 +10,28 @@ export default defineConfig({
         tsconfigPaths(),
         monkey({
             entry: "src/main.ts",
+            build: {
+                externalGlobals: {
+                    compressorjs: cdn.jsdelivr("Compressor"),
+                    turndown: cdn.jsdelivr(
+                        "TurndownService",
+                        "lib/turndown.browser.umd.js",
+                    ),
+                },
+            },
             userscript: {
                 name: {
-                    "": "Nowcoder Toolkit",
+                    "": "NowCoder Toolkit",
                     "zh-CN": "牛客网工具箱",
                 },
+                description: "复制问题为Markdown，保存为Jupyter Notebook",
                 author: "eclipher",
                 namespace: "https://github.com/eclipher/Userscripts",
-                match: ["*://www.nowcoder.com/practice/*"],
+                homepage:
+                    "https://github.com/eclipher/Userscripts/tree/main/userscripts/nowcoder-toolkit",
                 license: "MIT",
+                match: ["*://www.nowcoder.com/practice/*"],
+                icon: "https://www.google.com/s2/favicons?sz=64&domain=nowcoder.com",
                 grant: "GM_xmlhttpRequest",
             },
         }),
